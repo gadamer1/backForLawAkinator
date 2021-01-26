adf35c1aeb5f3e70b3267df237b72c6ca9af03ed
gadamer1 <gadamer1@gadamer1ui-MacBookPro.local>
2021-01-26 20:43:19 +0900

    s

--- app.js
+++ app.js
@@ -7,22 +7,28 @@ const io = require("socket.io")(http, {
     methods: ["GET", "POST"],
   },
 });
+
+const results = [
+  "판례 A와 연관이 있습니다",
+  "판례 B와 연관이 있습니다",
+  "판례 C와 연관이 있습니다",
+  ,
+  "판례 D와 연관이 있습니다",
+  ,
+  "판례 E와 연관이 있습니다",
+];
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 io.on("connection", (socket) => {
   console.log("a user connected");
-  socket.on("send message", (item) => {
-    const msg = item.name + ":" + item.message;
-    console.log(msg);
-    io.emit("receive message", { name: item.name, message: item.message });
-  });
+
   socket.on("request question", () => {
     console.log("request question");
     io.emit("receive question", "~와 연관이 있습니까?");
   });
   socket.on("answer", (number) => {
-    io.emit("result", "판례 A와 연관이 있습니다");
+    io.emit("result", results);
     console.log("number", number);
   });
   socket.on("disconnect", () => {
